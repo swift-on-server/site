03a180da5c6574ef1a104960c2c2e22c03a701c8
Joannis Orlandos <joannis@orlandos.nl>
2024-05-04 20:29:38 +0200

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -15,12 +15,23 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.1.2"),
         .package(url: "https://github.com/hummingbird-project/swift-mustache.git", from: "2.0.0-beta.1"),
+
+        // Articles
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-beta.4"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "__SwiftOnServer_org"
+            name: "__SwiftOnServer_org",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "Logging", package: "swift-log"),
+            ]
         ),
         .executableTarget(
             name: "SiteBuilder",
