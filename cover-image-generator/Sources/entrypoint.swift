5fd6da965d884b2eae85507cfcba1200bf5812ae
GErP83 <gurrka@gmail.com>
2025-06-04 09:25:04 +0200

    add new article and author

--- cover-image-generator/Sources/entrypoint.swift
+++ cover-image-generator/Sources/entrypoint.swift
@@ -86,7 +86,7 @@ struct CoverView: View {
 struct Entrypoint: AsyncParsableCommand {
 
     @Argument(help: "The input directory.")
-    var input: String = "../src/content/blog/posts"
+    var input: String = "../src/contents/blog/posts"
     
     @MainActor
     mutating func run() async throws {
